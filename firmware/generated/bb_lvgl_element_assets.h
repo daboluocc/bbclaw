e18a6e41dfe73741ec0985ad40b15f7eefd19c61
zhoushoujian <zhoushoujianwork@163.com>
2026-04-16 03:52:00 +0800

    feat: add generated font and UI assets to git
    - Add lv_font_bbclaw_cjk.c (CJK font)
    - Add bb_lvgl_element_assets.c/h (UI bitmaps)
    - Remove generated/ from firmware/.gitignore
    - Enables CI builds without local asset generation

---
+++ firmware/generated/bb_lvgl_element_assets.h
@@ -0,0 +1,14 @@
+#ifndef BB_LVGL_ELEMENT_ASSETS_H
+#define BB_LVGL_ELEMENT_ASSETS_H
+
+#include "lvgl.h"
+
+extern const lv_image_dsc_t bb_el_battery_frame_26x12;
+
+extern const lv_image_dsc_t bb_el_claw_mark_88;
+
+extern const lv_image_dsc_t bb_el_avatar_plate_64;
+
+extern const lv_image_dsc_t bb_el_mic_plate_88;
+
+#endif /* BB_LVGL_ELEMENT_ASSETS_H */
